40a0f2f3e0d8f6756f08426d936d196d965bdf54
Calvin Che <calvin@cczy.io>
2019-03-12 23:33:23 +0800

    Add CommandTag for postgres

--- PrismaDB-Result/ResultNonQueryResponse.cs
+++ PrismaDB-Result/ResultNonQueryResponse.cs
@@ -4,12 +4,24 @@
     {
         public long LastInsertId { get; set; }
         public int Warnings { get; set; }
+        public CommandTag Tag { get; set; }
 
-        public ResultNonQueryResponse(int rowsAffected = 0, long lastInsertId = 0, int warnings = 0) : base()
+        public ResultNonQueryResponse(int rowsAffected = 0, long lastInsertId = 0, int warnings = 0, CommandTag tag = CommandTag.INSERT) : base()
         {
             RowsAffected = rowsAffected;
             LastInsertId = lastInsertId;
             Warnings = warnings;
+        }
+
+        public enum CommandTag
+        {
+            INSERT,
+            DELETE,
+            UPDATE,
+            SELECT,
+            MOVE,
+            FETCH,
+            COPY
         }
     }
 }
