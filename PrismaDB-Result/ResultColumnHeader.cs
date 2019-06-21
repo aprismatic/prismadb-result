43da2bf26178ebdfe62d6ec61d06859069d93833
Calvin Che <calvin@cczy.io>
2019-06-21 16:25:59 +0800

    Type name serialization, version bump

--- PrismaDB-Result/ResultColumnHeader.cs
+++ PrismaDB-Result/ResultColumnHeader.cs
@@ -12,7 +12,44 @@ namespace PrismaDB.Result
         [DataMember]
         public int? MaxLength { get; set; }
 
-        public Type DataType { get; set; }
+        [DataMember]
+        public string TypeName { get; private set; }
+
+        private Type dataType;
+        public Type DataType
+        {
+            get
+            {
+                return dataType;
+            }
+            set
+            {
+                dataType = value;
+
+                if (value == typeof(string))
+                    TypeName = "string";
+                else if (value == typeof(short))
+                    TypeName = "short";
+                else if (value == typeof(int))
+                    TypeName = "int";
+                else if (value == typeof(long))
+                    TypeName = "long";
+                else if (value == typeof(float))
+                    TypeName = "float";
+                else if (value == typeof(double))
+                    TypeName = "double";
+                else if (value == typeof(decimal))
+                    TypeName = "decimal";
+                else if (value == typeof(DateTime))
+                    TypeName = "datetime";
+                else if (value == typeof(byte[]))
+                    TypeName = "byte[]";
+                else if (value == typeof(DBNull))
+                    TypeName = "null";
+                else
+                    TypeName = null;
+            }
+        }
 
         public ResultColumnHeader() : this("") { }
 
